66ab4e647e607e3535560c67fd0abe1181ddac83
Daseul SONG <ektmf363@naver.com>
2023-10-20 17:48:59 +0900

    first commit

---
+++ src/main/java/com/blog/BlogSearchApplication.java
@@ -0,0 +1,21 @@
+package com.blog;
+
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class BlogSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogSearchApplication.class, args);
+	}
+
+	@Bean
+	Hibernate5Module hibernate5Module() {
+		Hibernate5Module hibernate5Module = new Hibernate5Module();
+		//강제 지연 로딩 설정
+		hibernate5Module.configure(Hibernate5Module.Feature.FORCE_LAZY_LOADING, true);
+		return hibernate5Module;
+	}}
