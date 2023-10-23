16d6890bebce20975637ab516cdb41758972d30e
Daseul SONG <ektmf363@naver.com>
2023-10-23 16:25:48 +0900

    Redisson 적용
    동시 접근 문제 대비

--- src/main/java/com/blog/BlogSearchApplication.java
+++ src/main/java/com/blog/BlogSearchApplication.java
@@ -8,5 +8,4 @@ public class BlogSearchApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BlogSearchApplication.class, args);
 	}
-
 }
--- src/main/java/com/blog/service/BlogSearchServiceImpl.java
+++ src/main/java/com/blog/service/BlogSearchServiceImpl.java
@@ -9,7 +9,10 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -20,6 +23,7 @@ import java.net.URISyntaxException;
 import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import static java.util.stream.Collectors.toList;
 
@@ -30,44 +34,61 @@ public class BlogSearchServiceImpl implements BlogSearchService{
     private RankRepository rankRepository;
     @Autowired
     private JsonConverter jsonConverter;
+    @Autowired
+    private RedissonClient redissonClient;
+    @Value("${apiKey}")
+    private String apiKey;
 
-    public List<BlogSearchResponse> blogSearch(String keyword, Integer page) throws UnsupportedEncodingException, URISyntaxException, ParseException {
+    public List<BlogSearchResponse> blogSearch(String keyword, Integer page) throws UnsupportedEncodingException, URISyntaxException, ParseException, InterruptedException {
+        RLock rLock = redissonClient.getLock("RedissonLock");
 
-        // API 호출
-        RestTemplate rest = new RestTemplate();
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-
-        String apikey = "KakaoAK 2e9b1783227b82c6057068ec9a46116d";
-        headers.set("Authorization",apikey);
-
-        HttpEntity<String> entity = new HttpEntity<String>("parameters",headers);
-        String encode = URLEncoder.encode(keyword,"UTF-8");
-        String rawURI = "https://dapi.kakao.com/v2/search/blog?query="+encode+"?page="+page.toString();
-
-        URI uri = new URI(rawURI);
-
-        ResponseEntity<String> res = rest.exchange(uri, HttpMethod.GET, entity, String.class);
-
-        JSONParser jsonParser = new JSONParser();
-        JSONObject body = (JSONObject) jsonParser.parse(res.getBody().toString());
-        JSONArray docu = (JSONArray) body.get("documents");
-
-        if(docu.size() != 0){
-            BlogSearchResponse post = new BlogSearchResponse();
-            List<BlogSearchResponse> postList = new ArrayList<>();
-
-            for(int i=0; i<docu.size(); i++){
-                post = jsonConverter.convertToEntityAttribute(String.valueOf(docu.get(i)));
-                postList.add(post);
+        try {
+            boolean available = rLock.tryLock(5, 3, TimeUnit.SECONDS);
+            if (!available) {
+                return null;
             }
-            return postList;
+
+            // API 호출
+            RestTemplate rest = new RestTemplate();
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+
+            String apikey = "KakaoAK "+apiKey;
+            headers.set("Authorization",apikey);
+
+            HttpEntity<String> entity = new HttpEntity<String>("parameters",headers);
+            String encode = URLEncoder.encode(keyword,"UTF-8");
+            String rawURI = "https://dapi.kakao.com/v2/search/blog?query="+encode+"?page="+page.toString();
+
+            URI uri = new URI(rawURI);
+
+            ResponseEntity<String> res = rest.exchange(uri, HttpMethod.GET, entity, String.class);
+
+            JSONParser jsonParser = new JSONParser();
+            JSONObject body = (JSONObject) jsonParser.parse(res.getBody().toString());
+            JSONArray docu = (JSONArray) body.get("documents");
+
+            if(docu.size() != 0){
+                BlogSearchResponse post = new BlogSearchResponse();
+                List<BlogSearchResponse> postList = new ArrayList<>();
+
+                for(int i=0; i<docu.size(); i++){
+                    post = jsonConverter.convertToEntityAttribute(String.valueOf(docu.get(i)));
+                    postList.add(post);
+                }
+                return postList;
+            }
+
+            // add Keyword Count
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            throw new InterruptedException();
+        } finally {
+            rLock.unlock();
         }
-
-        // add Keyword Count
-
         return null;
     }
+
     public List<Rank> getRank() {
         List<Rank> ranks = rankRepository.getRank();
         return ranks;
